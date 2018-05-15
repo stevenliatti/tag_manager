701efdd1993b0e5f3123befc72dc8f88a09694e4
steven.liatti <steven.liatti@etu.hesge.ch>
2018-05-15 15:32:49 +0200

    Finally (!) check for existent tags when setting tags and do not overwrite them. Now, need to check if tag already present and do adding again.
    Improve error handling when get tags
    
    Need to improve delete case
    
    On branch master
    
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+extern crate xattr;
 extern crate clap;
 use clap::{App, Arg, ArgGroup};
 
-extern crate xattr;
+const ATTR_NAME : &str = "user.tags";
+const COMMA_ASCII_VALUE : u8 = 44;
+
+// TODO: where put type annotations in println ?
+fn str_to_vec(string: &str) -> Vec<&str> {
+    string.split(",").collect()
+}
 
 fn main() {
+    // TODO: improve help usage
     let matches = App::new("tag_manager").version("0.1.0").about("Manage your tags")
         .group(ArgGroup::with_name("ops").args(&["get", "set", "del"]))
         .arg(Arg::with_name("get")
@@ -32,36 +40,58 @@ fn main() {
         .get_matches();
 
     let files: Vec<&str> = matches.values_of("files").unwrap().collect();
-    
+
     // Get case
     if matches.is_present("get") {
-        // TODO: format when multiple tags (push in vector)
-        for file in &files {
-            if let Some(tags) = xattr::get(file, "user.tag").expect("Unable to get attributes") {
-                println!("tags {:?} for file {}", String::from_utf8(tags).unwrap(), file);
+        for &file in &files {
+            match xattr::get(file, ATTR_NAME) {
+                Ok(res) => match res {
+                    Some(tags) => println!("Tags {:?} for file \"{}\"",
+                        str_to_vec(&String::from_utf8(tags).unwrap()), file),
+                    None => println!("File \"{}\" has no tags", file)
+                },
+                Err(err) => println!("Error for file \"{}\" : {}", file, err)
             }
         }
     }
-        
+
     // Set case
     if let Some(tags) = matches.values_of("set") {
-        let tags: Vec<&str> = tags.collect();
-        // TODO: check if existent tag
-        for file in &files {
-            for tag in &tags {
-                xattr::set(file, "user.tag", tag.as_bytes())
-                    .expect("An error occurred when setting a tag");
+        let mut tags_args: Vec<&str> = tags.collect();
+
+        let mut new_tags_u8 : Vec<u8> = Vec::new();
+        for tag in &tags_args {
+            for u in tag.bytes() {
+                new_tags_u8.push(u);
             }
+            new_tags_u8.push(COMMA_ASCII_VALUE);
         }
-        println!("set tag(s) {:?} for file(s) {:?}", tags, files);
+
+        // TODO: if existent tag, do not duplicate
+        for &file in &files {
+            let mut update_tags_u8 = new_tags_u8.clone();
+            match xattr::get(file, ATTR_NAME) {
+                Ok(res) => match res {
+                    Some(get_tags) => 
+                        for tag in get_tags {
+                            update_tags_u8.push(tag);
+                        },
+                    None => { update_tags_u8.pop(); }
+                },
+                Err(err) => println!("Error for file \"{}\" : {}", file, err)
+            }
+
+            xattr::set(file, ATTR_NAME, &update_tags_u8).expect("Error when setting tags");
+            println!("Add/update tag(s) {:?} for file(s) {:?}", tags_args, files);
+        }
     }
 
     // Delete case
     if let Some(tags) = matches.values_of("del") {
         let tags: Vec<&str> = tags.collect();
         // TODO: remove only given tags, not the entire extended attribute
-        for file in &files {
-            xattr::remove(file, "user.tag").expect("An error occurred when deleting a tag");
+        for &file in &files {
+            xattr::remove(file, ATTR_NAME).expect("An error occurred when deleting a tag");
         }
         println!("del tag(s) {:?} for file(s) {:?}", tags, files);
     }
