9f37062f028b88e37961f21c61efaec71e12d488
steven.liatti <steven.liatti@etu.hesge.ch>
2018-05-16 18:27:55 +0200

    Refactor, close #2
    On branch master
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   src/lib.rs
    	modified:   src/main.rs

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+extern crate xattr;
+extern crate clap;
+
+const ATTR_NAME : &str = "user.tags";
+const COMMA_ASCII_VALUE : u8 = ',' as u8;
+
+enum Operation { Get, Set, Delete }
+use Operation::*;
+
+pub fn get_tags(files: &Vec<&str>) {
+    let mut empty : Vec<u8> = Vec::new();
+    for &file in files { check_existent_tags(file, &Get, &mut empty); }
+}
+
+pub fn set_tags(files: &Vec<&str>, tags: clap::Values) {
+    let tags: Vec<&str> = tags.collect();
+    let mut new_tags_u8 : Vec<u8> = Vec::new();
+    // Convert str tags to bytes
+    for tag in &tags {
+        for u in tag.bytes() { new_tags_u8.push(u); }
+        new_tags_u8.push(COMMA_ASCII_VALUE);
+    }
+
+    for &file in files {
+        let mut existent_tags = new_tags_u8.clone();
+        check_existent_tags(file, &Set, &mut existent_tags);
+        let mut full_tags_str = vec_u8_to_vec_string(existent_tags);
+        // Compute a Vec<String> of tags to check duplicata with dedup()
+        full_tags_str.sort();
+        full_tags_str.dedup();
+        xattr::set(file, ATTR_NAME, &vec_string_to_vec_u8(&full_tags_str))
+            .expect("Error when setting tag(s)");
+    }
+    println!("Tag(s) {:?} for file(s) {:?} have been setted", tags, files);
+}
+
+pub fn del_tags(files: &Vec<&str>, tags: clap::Values) {
+    let tags_to_del: Vec<&str> = tags.collect();
+    for &file in files {
+        let mut existent_tags = Vec::new();
+        check_existent_tags(file, &Delete, &mut existent_tags);
+        let mut existent_tags_str = vec_u8_to_vec_string(existent_tags);
+        // Retain tags that must be effectively deleted
+        for tag in &tags_to_del { existent_tags_str.retain(|ref e| e != &&tag.to_string()); }
+        // Delete the tags
+        xattr::remove(file, ATTR_NAME).expect("Error when deleting tag(s)");
+        // Adding again the tags that are not in tags_to_del
+        let final_tags = vec_string_to_vec_u8(&existent_tags_str);
+        xattr::set(file, ATTR_NAME, &final_tags).expect("Error when (re)setting tag(s)");
+    }
+    println!("Tag(s) {:?} for file(s) {:?} have been deleted", tags_to_del, files);
+}
+
+// TODO: where put type annotations in println ?
+fn str_to_vec(string: &str) -> Vec<&str> {
+    string.split(",").collect()
+}
+
+fn check_existent_tags(file: &str, operation: &Operation, existent_tags: &mut Vec<u8>) {
+    match xattr::get(file, ATTR_NAME) {
+        Ok(res) => match res {
+            Some(tags) => {
+                match operation {
+                    &Get => println!("Tags {:?} for file \"{}\"", 
+                        str_to_vec(&String::from_utf8(tags).unwrap()), file),
+                    _ => for tag in tags { existent_tags.push(tag); }
+                }
+            },
+            None => {
+                match operation {
+                    &Set => { existent_tags.pop(); },
+                    _ => { println!("File \"{}\" has no tags", file) }
+                }
+            }
+        },
+        Err(err) => { 
+            println!("Error for file \"{}\" : {}", file, err);
+            return;
+        }
+    }
+}
+
+fn vec_string_to_vec_u8(tags_string: &Vec<String>) -> Vec<u8> {
+    let mut tags_u8 : Vec<u8> = Vec::new();
+    for tag in tags_string {
+        for u in tag.bytes() { tags_u8.push(u); }
+        tags_u8.push(COMMA_ASCII_VALUE);
+    }
+    // remove last comma
+    tags_u8.pop();
+    tags_u8
+}
+
+fn vec_u8_to_vec_string(tags_u8: Vec<u8>) -> Vec<String> {
+    let mut s = String::new();
+    let mut tags_str : Vec<String> = Vec::new();
+    for u in tags_u8 {
+        if u == COMMA_ASCII_VALUE {
+            tags_str.push(s.to_string());
+            s = String::new();
+        }
+        else { s.push(u as char); }
+    }
+    tags_str.push(s.to_string());
+    tags_str
+}
--- src/main.rs
+++ src/main.rs
@@ -1,178 +1,22 @@
-extern crate xattr;
+extern crate tag_manager;
 extern crate clap;
 use clap::{App, Arg, ArgGroup};
-
-const ATTR_NAME : &str = "user.tags";
-const COMMA_ASCII_VALUE : u8 = ',' as u8;
-
-// TODO: where put type annotations in println ?
-fn str_to_vec(string: &str) -> Vec<&str> {
-    string.split(",").collect()
-}
 
 fn main() {
     // TODO: improve help usage
     let matches = App::new("tag_manager").version("0.1.0").about("Manage your tags")
         .group(ArgGroup::with_name("ops").args(&["get", "set", "del"]))
-        .arg(Arg::with_name("get")
-            .help("Get tag(s)")
-            .short("g")
-            .long("get"))
-        .arg(Arg::with_name("set")
-            .help("Set tag(s)")
-            .short("s")
-            .long("set")
-            .takes_value(true)
-            .multiple(true))
-        .arg(Arg::with_name("del")
-            .help("Delete tag(s)")
-            .short("d")
-            .long("del")
-            .takes_value(true)
-            .multiple(true))
-        .arg(Arg::with_name("files")
-            .help("File(s) name(s)")
-            .short("f")
-            .long("files")
-            .takes_value(true)
-            .required(true)
-            .multiple(true))
+        .arg(Arg::with_name("get").help("Get tag(s)").short("g").long("get"))
+        .arg(Arg::with_name("set").help("Set tag(s)").short("s").long("set")
+            .takes_value(true).multiple(true))
+        .arg(Arg::with_name("del").help("Delete tag(s)").short("d").long("del")
+            .takes_value(true).multiple(true))
+        .arg(Arg::with_name("files").help("File(s) name(s)").short("f").long("files")
+            .takes_value(true).multiple(true).required(true))
         .get_matches();
 
     let files: Vec<&str> = matches.values_of("files").unwrap().collect();
-
-    // Get case
-    if matches.is_present("get") {
-        for &file in &files {
-            match xattr::get(file, ATTR_NAME) {
-                Ok(res) => match res {
-                    Some(tags) => println!("Tags {:?} for file \"{}\"",
-                        str_to_vec(&String::from_utf8(tags).unwrap()), file),
-                    None => println!("File \"{}\" has no tags", file)
-                },
-                Err(err) => println!("Error for file \"{}\" : {}", file, err)
-            }
-        }
-    }
-
-    // Set case
-    if let Some(tags) = matches.values_of("set") {
-        let mut tags: Vec<&str> = tags.collect();
-        let mut new_tags_u8 : Vec<u8> = Vec::new();
-
-        // Convert str tags to bytes
-        for tag in &tags {
-            for u in tag.bytes() {
-                new_tags_u8.push(u);
-            }
-            new_tags_u8.push(COMMA_ASCII_VALUE);
-        }
-
-        for &file in &files {
-            let mut update_tags_u8 = new_tags_u8.clone();
-            match xattr::get(file, ATTR_NAME) {
-                Ok(res) => match res {
-                    Some(get_tags) =>
-                        for tag in get_tags {
-                            update_tags_u8.push(tag);
-                        },
-                    None => { update_tags_u8.pop(); }
-                },
-                Err(err) => println!("Error for file \"{}\" : {}", file, err)
-            }
-
-            let mut s = String::new();
-            let mut full_tags_str : Vec<String> = Vec::new();
-            for u in update_tags_u8.clone() {
-                if u == COMMA_ASCII_VALUE {
-                    full_tags_str.push(s.to_string());
-                    s = String::new();
-                }
-                else {
-                    s.push(u as char);
-                }
-            }
-            full_tags_str.push(s.to_string());
-            full_tags_str.sort();
-            full_tags_str.dedup();
-
-            let mut final_tags : Vec<u8> = Vec::new();
-            // Convert str tags to bytes
-            for tag in &full_tags_str {
-                for u in tag.bytes() {
-                    final_tags.push(u);
-                }
-                final_tags.push(COMMA_ASCII_VALUE);
-            }
-            final_tags.pop();
-            xattr::set(file, ATTR_NAME, &final_tags).expect("Error when setting tag(s)");
-        }
-        println!("Add tag(s) {:?} for file(s) {:?}", tags, files);
-    }
-
-    // Delete case
-    if let Some(tags) = matches.values_of("del") {
-        let tags_to_del: Vec<&str> = tags.collect();
-        let mut new_tags_u8 : Vec<u8> = Vec::new();
-
-        // Convert str tags to bytes
-        for tag in &tags_to_del {
-            for u in tag.bytes() {
-                new_tags_u8.push(u);
-            }
-            new_tags_u8.push(COMMA_ASCII_VALUE);
-        }
-
-        for &file in &files {
-            let mut existent_tags = Vec::new();
-            match xattr::get(file, ATTR_NAME) {
-                Ok(res) => match res {
-                    Some(get_tags) =>
-                        for tag in get_tags {
-                            existent_tags.push(tag);
-                        },
-                    None => println!("File \"{}\" has no tags", file)
-                },
-                Err(err) => {
-                    println!("Error for file \"{}\" : {}", file, err);
-                    return;
-                }
-            }
-
-            // Compute a Vec<String> to check existent tags
-            let mut s = String::new();
-            let mut existent_tags_str : Vec<String> = Vec::new();
-            for u in existent_tags {
-                if u == COMMA_ASCII_VALUE {
-                    existent_tags_str.push(s.to_string());
-                    s = String::new();
-                }
-                else {
-                    s.push(u as char);
-                }
-            }
-            existent_tags_str.push(s.to_string());
-
-            // Retain tags that must be effectively deleted
-            for tag in &tags_to_del {
-                existent_tags_str.retain(|ref e| e != &&tag.to_string());
-            }
-
-            // Delete the tags
-            xattr::remove(file, ATTR_NAME).expect("Error when deleting tag(s)");
-
-            // Readd the tags that are not in tags_to_del
-            let mut update_tags_u8 : Vec<u8> = Vec::new();
-            for tag in &existent_tags_str {
-                for u in tag.bytes() {
-                    update_tags_u8.push(u);
-                }
-                update_tags_u8.push(COMMA_ASCII_VALUE);
-            }
-            // remove last comma
-            update_tags_u8.pop();
-            xattr::set(file, ATTR_NAME, &update_tags_u8).expect("Error when (re)setting tag(s)");
-        }
-        println!("Delete tag(s) {:?} for file(s) {:?}", tags_to_del, files);
-    }
+    if matches.is_present("get") { tag_manager::get_tags(&files); }
+    if let Some(tags) = matches.values_of("set") { tag_manager::set_tags(&files, tags); }
+    if let Some(tags) = matches.values_of("del") { tag_manager::del_tags(&files, tags); }
 }
