655a80faccf6f18f9a4d2239869cdcf9fcf790a1
steven.liatti <steven.liatti@etu.hesge.ch>
2018-05-18 16:22:53 +0200

    Fix a bug and begin tests
    On branch master
    
    Changes to be committed:
    	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,9 @@ pub fn set_tags(files: &Vec<&str>, tags: clap::Values) {
         for u in tag.bytes() { new_tags_u8.push(u); }
         new_tags_u8.push(SEPARATOR);
     }
-
     for &file in files {
         let mut existent_tags = new_tags_u8.clone();
+        // If needed, add existent tags to existent_tags in check_existent_tags()
         check_existent_tags(file, &Set, &mut existent_tags);
         let mut full_tags_str = vec_u8_to_vec_string(existent_tags);
         // Compute a Vec<String> of tags to check duplicata with dedup()
@@ -38,15 +38,17 @@ pub fn del_tags(files: &Vec<&str>, tags: clap::Values) {
     let tags_to_del: Vec<&str> = tags.collect();
     for &file in files {
         let mut existent_tags = Vec::new();
+        // If needed, add existent tags to existent_tags in check_existent_tags()
         check_existent_tags(file, &Delete, &mut existent_tags);
         let mut existent_tags_str = vec_u8_to_vec_string(existent_tags);
         // Retain tags that must be effectively deleted
         for tag in &tags_to_del { existent_tags_str.retain(|ref e| e != &&tag.to_string()); }
-        // Delete the tags
-        xattr::remove(file, ATTR_NAME).expect("Error when deleting tag(s)");
-        // Adding again the tags that are not in tags_to_del
-        let final_tags = vec_string_to_vec_u8(&existent_tags_str);
-        xattr::set(file, ATTR_NAME, &final_tags).expect("Error when (re)setting tag(s)");
+        if existent_tags_str.is_empty() { match xattr::remove(file, ATTR_NAME) { _ => () } }
+        else {
+            // Adding again the tags that are not in tags_to_del
+            xattr::set(file, ATTR_NAME, &vec_string_to_vec_u8(&existent_tags_str))
+                .expect("Error when (re)setting tag(s)");
+        }
     }
     println!("Tag(s) {:?} for file(s) {:?} have been deleted", tags_to_del, files);
 }
@@ -67,7 +69,7 @@ fn check_existent_tags(file: &str, operation: &Operation, existent_tags: &mut Ve
                 }
             }
         },
-        Err(err) => { 
+        Err(err) => {
             println!("Error for file \"{}\" : {}", file, err);
             return;
         }
@@ -76,25 +78,66 @@ fn check_existent_tags(file: &str, operation: &Operation, existent_tags: &mut Ve
 
 fn vec_string_to_vec_u8(tags_string: &Vec<String>) -> Vec<u8> {
     let mut tags_u8 : Vec<u8> = Vec::new();
-    for tag in tags_string {
-        for u in tag.bytes() { tags_u8.push(u); }
-        tags_u8.push(SEPARATOR);
+    if !tags_string.is_empty() {
+        for tag in tags_string {
+            for u in tag.bytes() { tags_u8.push(u); }
+            tags_u8.push(SEPARATOR);
+        }
+        // remove last comma
+        tags_u8.pop();
     }
-    // remove last comma
-    tags_u8.pop();
     tags_u8
 }
 
 fn vec_u8_to_vec_string(tags_u8: Vec<u8>) -> Vec<String> {
     let mut s = String::new();
     let mut tags_str : Vec<String> = Vec::new();
-    for u in tags_u8 {
-        if u == SEPARATOR {
-            tags_str.push(s.to_string());
-            s = String::new();
+    if !tags_u8.is_empty() {
+        for u in tags_u8 {
+            if u == SEPARATOR {
+                tags_str.push(s.to_string());
+                s = String::new();
+            }
+            else { s.push(u as char); }
         }
-        else { s.push(u as char); }
+        tags_str.push(s.to_string());
     }
-    tags_str.push(s.to_string());
     tags_str
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn vec_u8_to_vec_string() {
+        let empty_u8 : Vec<u8> = Vec::new();
+        let empty_string : Vec<String> = Vec::new();
+        assert_eq!(empty_string, super::vec_u8_to_vec_string(empty_u8));
+
+        // ["ACDC"]
+        let vec_u8 : Vec<u8> = vec![65, 67, 68, 67];
+        let vec_string : Vec<String> = vec!["ACDC".to_string()];
+        assert_eq!(vec_string, super::vec_u8_to_vec_string(vec_u8));
+
+        // ["ACDC", "BOB"]
+        let vec_u8 : Vec<u8> = vec![65, 67, 68, 67, super::SEPARATOR, 66, 79, 66];
+        let vec_string : Vec<String> = vec!["ACDC".to_string(), "BOB".to_string()];
+        assert_eq!(vec_string, super::vec_u8_to_vec_string(vec_u8));
+    }
+
+    #[test]
+    fn vec_string_to_vec_u8() {
+        let empty_u8 : Vec<u8> = Vec::new();
+        let empty_string : Vec<String> = Vec::new();
+        assert_eq!(empty_u8, super::vec_string_to_vec_u8(&empty_string));
+
+        // ["ACDC"]
+        let vec_u8 : Vec<u8> = vec![65, 67, 68, 67];
+        let vec_string : Vec<String> = vec!["ACDC".to_string()];
+        assert_eq!(vec_u8, super::vec_string_to_vec_u8(&vec_string));
+
+        // ["ACDC", "BOB"]
+        let vec_u8 : Vec<u8> = vec![65, 67, 68, 67, super::SEPARATOR, 66, 79, 66];
+        let vec_string : Vec<String> = vec!["ACDC".to_string(), "BOB".to_string()];
+        assert_eq!(vec_u8, super::vec_string_to_vec_u8(&vec_string));
+    }
 }
